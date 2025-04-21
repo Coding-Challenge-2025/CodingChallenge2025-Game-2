d5e3a601826cda9c4d3a5aa12a69ff69d4f4c2be
zeeptobean <zeeptobean@gmail.com>
2025-04-21 21:17:02 +0700

    add simple sent question/receive answer in loop; randomized room id

--- server/src/app.js
+++ server/src/app.js
@@ -12,11 +12,41 @@ import {
 
 const MAX_PLAYER = 4;
 const port = 3000;
-const clientList = new Map();
+const clientList = new Map();               //store websocket object & name
+const clientWorkingStateList = new Map();   //store boolean client is answering
+const clientAnswerList = new Map();         //consists of answer & client answer time 
+const clientScoreList = new Map();         //store client's answer
+const clientMessageListenerList = new Map();
+const clientTimeoutList = new Map();
+const ANSWER_TIMEOUT = 30000 
 
-const serverRoomId = "7761AA"
+let serverRoomId
 
 let currentPlayerCount = 0
+
+function getRandomRoomId(len) {
+    const charlist = 'ABCDEF0123456789';
+    let result = '';
+    for (let i = 0; i < len; i++) {
+        result += charlist.charAt(Math.floor(Math.random() * charlist.length));
+    }
+    return result;
+}
+
+function getRandomIndex(arr) {
+    if (!arr || arr.length === 0) {
+        return undefined; // Handle empty or non-array input
+    }
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return randomIndex;
+}
+
+function randomShuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
 
 async function boardcastClientWaitingForSignal() {
     return setInterval(() => {
@@ -38,14 +68,72 @@ async function boardcastClient(object) {
     }
 }
 
+async function broadcastQuestion(questionObject) {
+    //Every resolve call mark each client completion
+    const sendPromises = Array.from(clientList).map(([wsObject, playerName]) => {
+        if (wsObject.readyState !== WebSocket.OPEN) {
+            return Promise.resolve();
+        }
+        return new Promise(resolve => {
+            wsObject.send(JSON.stringify(questionObject));
+            clientAnswerList.set(wsObject, null);
+            clientWorkingStateList.set(wsObject, true);
+            console.log(`Question sent to player ${playerName}`)
+
+            // const innerMessageListener = (playerAnswer) => {
+            //     clientAnswerList.set(wsObject, JSON.parse(playerAnswer));
+            //     console.log(`Player ${clientList.get(wsObject)} answered: ${playerAnswer}`);
+            //     wsObject.off('message', innerMessageListener);
+            //     clientMessageListenerList.set(wsObject, null);
+            //     clientWorkingStateList.set(wsObject, false);
+            // };
+
+            // wsObject.once('message', innerMessageListener);
+            // clientMessageListenerList.set(wsObject, innerMessageListener);
+            // clientTimeoutList.set(wsObject, setTimeout(() => {
+            //     clientTimeoutList.set(wsObject, null);
+            //     clientAnswerList.set(wsObject, { answer: "", time: 0 });
+            //     console.log(`Player ${clientList.get(wsObject)} timeouted:`);
+            // }, 20000));
+
+            const timeoutId = setTimeout(() => {
+                console.log(`No response from client ${playerName}`);
+                clientWorkingStateList.set(wsObject, false);
+                resolve()
+            }, ANSWER_TIMEOUT);
+
+            wsObject.once('message', clientAnswer => {
+                clearTimeout(timeoutId);
+                clientWorkingStateList.set(wsObject, false);
+                try {
+                    const clientAnswerJson = JSON.parse(clientAnswer.toString());
+                    clientAnswerList.set(wsObject, clientAnswerJson);
+                    console.log(`Answer from client ${playerName}: ${clientAnswerJson["answer"]}`);
+                } catch(error) {
+                    console.error(`Error parsing answer from client ${playerName}: `, error);
+                }
+                resolve();
+            });
+        })
+    })
+
+    // Await *all* the client‑promises before returning
+    await Promise.all(sendPromises);
+}
+
 app.ws("/", (ws, req) => {
     let clientRemoveAddress = req.socket.remoteAddress
-    console.log(`A client has connected from address ${clientRemoveAddress}`);
+    console.log(`A client has connected from address ${clientRemoveAddress}. Waiting for identification`);
 
     ws.on("close", () => {
-        if(clientList.has(ws)) {
+        if (clientList.has(ws)) {
             console.log(`Player ${clientList.get(ws)} disconnected`);
             clientList.delete(ws);
+            clientWorkingStateList.delete(ws);
+            clientAnswerList.delete(ws);
+            clientScoreList.delete(ws);
+            clientMessageListenerList.delete(ws);
+            clientTimeoutList.delete(ws);
             currentPlayerCount--;
         }
     });
@@ -54,11 +142,11 @@ app.ws("/", (ws, req) => {
         console.error("WebSocket error:", err);
     });
 
-    ws.on('message', (msg) => {
+    ws.once('message', (msg) => {
         let jsonData
         try {
             jsonData = JSON.parse(msg);
-        } catch(error) {
+        } catch (error) {
             console.error("Error parsing JSON:", error.message);
             return
         }
@@ -68,9 +156,12 @@ app.ws("/", (ws, req) => {
 
         console.log(`Client name: ${clientName}\nClient ID: ${roomId}`);
 
-        if(roomId == serverRoomId) {
-            if(currentPlayerCount <= MAX_PLAYER) {
+        if (roomId == serverRoomId) {
+            if (currentPlayerCount <= MAX_PLAYER) {
                 clientList.set(ws, clientName)
+                clientWorkingStateList.set(ws, false);
+                clientAnswerList.set(ws, undefined);
+                clientScoreList.set(ws, 0);
                 console.log(`Player ${clientName} authorized`);
                 ws.send("Authorized!");
                 currentPlayerCount++;
@@ -88,29 +179,30 @@ app.ws("/", (ws, req) => {
 });
 
 app.listen(port, () => {
+    serverRoomId = getRandomRoomId(6);
     console.log(`WebSocket server listening on port ${port}`);
+    console.log(`Conenct with room ID ${serverRoomId}`);
 });
 
 const clientResponseList = [];
 
 async function initGame() {
-    await loadKeywordsList(process.cwd() + "/server/assets/keywords.json")
-    await loadQuestionsList(process.cwd() + "/server/assets/questions.json")
+    // await loadKeywordsList(process.cwd() + "/server/assets/keywords.json")
+    // await loadQuestionsList(process.cwd() + "/server/assets/questions.json")
+    await loadKeywordsList(process.cwd() + "/assets/keywords.json")
+    await loadQuestionsList(process.cwd() + "/assets/questions.json")
     //Reserve for response array
     clientResponseList.length = 0
-    for(let i=0; i < MAX_PLAYER; i++) {
+    for (let i = 0; i < MAX_PLAYER; i++) {
         clientResponseList[i] = '';
     }
     //Empty players
     currentPlayerCount = 0;
     clientList.length = 0;
     //Shuffle keywords clues
-    for(const ele of keywordsList) {
+    for (const ele of keywordsList) {
         let innerCluesList = ele["clues"];      //assignment on array = reference, use = [...oldarr] to shallow copy
-        for(let i = innerCluesList.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [innerCluesList[i], innerCluesList[j]] = [innerCluesList[j], innerCluesList[i]];
-        }
+        randomShuffleArray(innerCluesList)
     }
 }
 
@@ -118,26 +210,68 @@ async function sendQuestion() {
     const randomIndex = Math.floor(Math.random() * keywordsList.length);
     const question_selected = questionsList[randomIndex];
     questionsList.splice(randomIndex, 1);
-    boardcastClient(question_selected)
+    await boardcastClient(question_selected)
 }
 
 async function sendKeyword() {
     const randomIndex = Math.floor(Math.random() * keywordsList.length);
     const keyword_selected = keywordsList[randomIndex];
     keywordsList.splice(randomIndex, 1);
-    boardcastClient(keyword_selected)
+    await boardcastClient(keyword_selected)
 }
 
 async function choosePiece(params) {
-    
+
+}
+
+async function ingame() {
+    for (let i = 0; i < 12; i++) {
+        let givenIndex = getRandomIndex(questionsList);
+        let questionObject = { ...questionsList[givenIndex] };
+        questionsList.splice(givenIndex, 1);
+        questionObject.answer_index = undefined;
+        randomShuffleArray(questionObject.choice);
+        await broadcastQuestion(questionObject);
+        //for-loop is still synchronous, need to hold back
+        //In practice there would be a button to choose question from UI, this is for testing
+        const delayBetweenQuestion = 3000;
+        console.log(`Cooldown between question: ${delayBetweenQuestion}ms`)
+        await new Promise(dummy => setTimeout(dummy, delayBetweenQuestion));
+    }
 }
 
 async function startGame() {
-    if(currentPlayerCount == MAX_PLAYER) {
+    // if(currentPlayerCount == MAX_PLAYER) {
 
-    } else console.log("Not enough players to start!");
+    // } else console.log("Not enough players to start!");
+
+    
+    let intervalHandle = setInterval(() => {
+        for (let client of clientList) {
+            if (client[0].readyState === WebSocket.OPEN) {
+                client[0].send("Waiting for event...");
+            }
+        }
+        if (currentPlayerCount > 0) {
+            clearInterval(intervalHandle);
+            console.log("Starting game...")
+            for (let client of clientList) {
+                if (client[0].readyState === WebSocket.OPEN) {
+                    client[0].send("Starting game...!");
+                }
+            }
+            ingame();       //setInterval is async, need to make function call to execute synchorously
+        }
+    }, 3000)
+
+
 }
 
-initGame();
+async function main() {
+    await initGame();
+    await startGame();
+}
+
+main();
 //placeholder
-let intervalHandle = boardcastClientWaitingForSignal();
+// let intervalHandle = boardcastClientWaitingForSignal();
