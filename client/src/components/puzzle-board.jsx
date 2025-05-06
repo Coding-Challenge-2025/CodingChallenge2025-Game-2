c1ae55a855afab6f4dbbd1cd9e50364b324af7e5
powerstomp <119428043+powerstomp@users.noreply.github.com>
2025-05-06 22:15:19 +0700

    puzzle hide fix

--- client/src/components/puzzle-board.jsx
+++ client/src/components/puzzle-board.jsx
@@ -3,9 +3,9 @@ import { Badge } from "@/components/ui/badge"
 
 export default function PuzzleBoard({ revealed, imageData }) {
   return (
-    <div className="relative mx-auto aspect-[16/9] w-full max-w-4xl overflow-hidden bg-red-400 grid grid-cols-4 grid-rows-3">
-      <img alt="Round image" className="absolute inset-0 h-full w-full object-cover" src={imageData} />
-
+    <div className="relative mx-auto aspect-[16/9] w-full max-w-4xl overflow-hidden bg-red-400 grid grid-cols-4 grid-rows-3 bg-cover bg-center"
+      style={{ backgroundImage: `url(${imageData})` }}
+    >
       {revealed.map((value, index) =>
         <div key={index} className={clsx(
           "z-10 flex items-center justify-center border border-blue-500 bg-blue-400",
