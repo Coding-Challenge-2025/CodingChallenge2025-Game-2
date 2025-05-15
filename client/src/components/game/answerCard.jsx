e5be544073eb6313669118b8e28586da727a4bd1
powerstomp <119428043+powerstomp@users.noreply.github.com>
2025-05-15 09:03:29 +0700

    Add markdown and mathjax support

--- client/src/components/game/answerCard.jsx
+++ client/src/components/game/answerCard.jsx
@@ -1,11 +1,18 @@
 import Panel from "./panel";
 import AnswerPanel from "./answerPanel"
 import Timer from "@/components/timer"
+import Markdown from "react-markdown"
+import remarkMath from "remark-math"
+import rehypeMathjax from "rehype-mathjax"
 
 export default function AnswerCard({ children, currentQuestion, submitAnswer, timeLeft }) {
 	return (
 		<Panel title={`Question ${currentQuestion.id + 1}`} customStatus={<Timer timeLeft={timeLeft} />}>
-			<h3 className="text-lg p-4">{currentQuestion.text}</h3>
+			<h3 className="text-lg p-4">
+				<Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeMathjax]}>
+					{currentQuestion.text}
+				</Markdown>
+			</h3>
 			<AnswerPanel submitAnswer={timeLeft > 0 ? submitAnswer : undefined} />
 			{children}
 		</Panel>
