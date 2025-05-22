affad0084837b605e469a858b1f3ad066666bdaa
powerstomp <119428043+powerstomp@users.noreply.github.com>
2025-05-22 07:57:27 +0700

    Add header image

--- client/src/components/gameLayout.jsx
+++ client/src/components/gameLayout.jsx
@@ -4,27 +4,33 @@ import clsx from "clsx"
 
 export default function GameLayout({ children, revealed, imageData, notifications, isPlayer = true }) {
   return (
-    <div className={clsx(
-      "min-h-screen max-w-full mx-auto p-2 grid grid-cols-1 md:grid-cols-2 gap-2",
-      !isPlayer && "items-center")}>
-      {/* Left column - Puzzle */}
-      <div className="space-y-2">
-        <Panel title="Puzzle Board">
-          <PuzzleBoard revealed={revealed} imageData={imageData} />
-        </Panel>
-        {notifications?.length > 0 &&
-          <Panel title="Notifications">
-            {notifications.map((value, id) =>
-              <div key={value.time + value.message}>{value.time.toLocaleString()} - {value.message}</div>
-            )}
-          </Panel>
-        }
-      </div>
+    <>
+      <div className={clsx(
+        "min-h-screen max-w-full mx-auto p-2 gap-2",
+        !isPlayer && "items-center")}>
+        <img src="/header2.png" className="mx-auto mb-4 w-2/3" />
+        <div className={clsx("grid grid-cols-1 md:grid-cols-2",
+          !isPlayer && "items-center")}>
+          {/* Left column - Puzzle */}
+          <div className="space-y-2">
+            <Panel title="Puzzle Board">
+              <PuzzleBoard revealed={revealed} imageData={imageData} />
+            </Panel>
+            {notifications?.length > 0 &&
+              <Panel title="Notifications">
+                {notifications.map((value, id) =>
+                  <div key={value.time + value.message}>{value.time.toLocaleString()} - {value.message}</div>
+                )}
+              </Panel>
+            }
+          </div>
 
-      {/* Right column - Questions and answers */}
-      <div className="space-y-2">
-        {children}
+          {/* Right column - Questions and answers */}
+          <div className="space-y-2">
+            {children}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
